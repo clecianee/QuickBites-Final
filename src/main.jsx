da76d5736da33ce73b2a0b3752cd480099916fef
izbethgarcia <lizbeth.garcia@bellevuecollege.edu>
2026-03-18 13:42:28 -0700

    Reorder imports and component structure in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
-import "./styles/app.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./styles/app.css";
+
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
+    <AuthProvider>
+      <BrowserRouter>
         <App />
-      </AuthProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
